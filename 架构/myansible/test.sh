0b002825f31b0188e0b5fe98eba1d1cb27c9e498
root <root@room9pc01.tedu.cn>
2018-11-22 22:15:46 +0800

    学习正常提交

--- 架构/myansible/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo '
-{
-  "aa" : ["web1", "web2"],
-  "bb" : ["db1", "db2"]
-}
-'
--- 模板机制作及脚本/createVM残次品.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#read -p "input VM number:" VMNUM
-cd /var/lib/libvirt/images/
-#qemu-img create -b openstack.qcow2 -f qcow2 openstack.img 16G
-sed "s#node#nova01#" /root/桌面/nsd1807/node.xml > /etc/libvirt/qemu/nova01.xml
-virsh define /etc/libvirt/qemu/nova01.xml
-#virsh start nova01$VMNUM
